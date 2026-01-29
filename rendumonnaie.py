b57c4e00b71bbeaa04593dfe5d0659bc23b91c59
Caerroff <caerroff@proton.me>
2026-01-29 17:23:26 +0200

    cleanup

--- rendumonnaie.py
+++ rendumonnaie.py
@@ -1,26 +1,26 @@
-systeme = []
-for x in range(2,-1,-1): 
-    systeme += 5 * (10**x),2 * (10**x),1 * (10**x) # ? * (10**x)
-print(systeme)
+systeme = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+solution = []
 etape = 0
 max = 101
-for i in range(1,max):
-    print("-------ON TESTE ",i,"-------")
-    etapeEnCours = 1
-    payer = i 
-    k = 0
-    x = 0
-    while payer != 0: 
-        if systeme[x] > payer:
-            x +=1
-        elif systeme[x] <= payer:
-            print("étape n° ", etapeEnCours)
-            print("reste ",payer," monnaie : ",systeme[x])
-            payer -= systeme[x]
-            etape +=1
-            etapeEnCours +=1
+i = 59.73
+print("-------ON TESTE ",i,"-------")
+etapeEnCours = 1
+reste = i 
+k = 0
+x = 0
+while reste != 0: 
+    if systeme[x] > reste:
+        x +=1
+    elif systeme[x] <= reste:
+        print("étape n° ", etapeEnCours)
+        print("reste ",reste," monnaie : ",systeme[x])
+        reste -= systeme[x]
+        reste = round(reste,2)
+        solution.append(systeme[x])
+        print("il reste à reste : ",reste)
+        etape +=1
+        etapeEnCours +=1
         
-
-print(etape)
-moyenneEtape = etape / (max-1)
-print(moyenneEtape)
+print("-------FINI-------")
+print("Etapes : ",etape)
+print("Solution : ",solution)
